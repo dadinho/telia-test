3a70ac2c83acf91cc51703700278ae5b7267a99a
László Bottlik <laszlo.bottlik@outlook.com>
2019-08-13 10:53:48 +0200

    Create static version.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import './Reset.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="player">
+        <video controls name="media">
+          <source src="http://mirrors.standaloneinstaller.com/video-sample/dolbycanyon.mp4" type="video/mp4"></source>
+          Your browser does not support HTML5 video.
+        </video>
+      </div>
+      <div className="list">
+        <div className="item">
+          <img className="thumbnail" src="https://iptvimagecache-playplus-prod-vip.han.telia.se/crop/200x290/http%3A%2F%2Fiptvlogin.telia.se%2F%2FSF%20Anytime%2F4749441.jpg" />
+          <span className="title">Aquaman</span>
+        </div>
+        <div className="item">
+          <img className="thumbnail" src="https://iptvimagecache-playplus-prod-vip.han.telia.se/crop/200x290/http%3A%2F%2Fiptvlogin.telia.se%2F%2FSF%20Anytime%2F4749441.jpg" />
+          <span className="title">Aquaman</span>
+        </div>
+        <div className="item">
+          <img className="thumbnail" src="https://iptvimagecache-playplus-prod-vip.han.telia.se/crop/200x290/http%3A%2F%2Fiptvlogin.telia.se%2F%2FSF%20Anytime%2F4749441.jpg" />
+          <span className="title">Aquaman</span>
+        </div>
+        <div className="item">
+          <img className="thumbnail" src="https://iptvimagecache-playplus-prod-vip.han.telia.se/crop/200x290/http%3A%2F%2Fiptvlogin.telia.se%2F%2FSF%20Anytime%2F4749441.jpg" />
+          <span className="title">Aquaman</span>
+        </div>
+      </div>
     </div>
   );
 }
